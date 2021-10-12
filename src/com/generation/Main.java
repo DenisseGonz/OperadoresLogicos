20383776c940171893b306f0270003f2192f4141
Denisse Gonzalez <denisse.gonz.r@gmail.com>
2021-10-12 17:40:40 -0500

    Clase

---
+++ src/com/generation/Main.java
@@ -0,0 +1,24 @@
+package com.generation;
+
+public class Main {
+    public static void main(String [] args){
+        boolean x,y,z;
+        x=true;
+        y=false;
+        z=true;
+
+        boolean a,b,c,d,e,f;
+        a= (x&&y) || (x&&z);
+        b=(x||!y) && (!x||z);
+        c=x || y && z;
+        d=!( x || y ) && z;
+        e=x || y || x && !z && !y;
+        f=!x || !y || z && x && !y;
+        System.out.println("a) "+ a );
+        System.out.println("b) "+ b );
+        System.out.println("c) "+ c);
+        System.out.println("d) "+ d);
+        System.out.println("e) "+ e);
+        System.out.println("f) "+ f);
+    }
+}
